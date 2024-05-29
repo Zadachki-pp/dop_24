1f372d4db03e7aaa392bdb0f26596302fb63f80a
LEv145 <you@example.com>
2024-05-29 02:03:16 +0200

    Обновлён код с большим учётом физики

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 #include <chrono>
 #include <thread>
 
@@ -10,10 +10,12 @@ enum class VectorDirection {up, down};
 
 
 float SHAPE_RADIUS = 30;
-float C = 100;  // Коэфицент соправтивление воздуху
-float G = 9.8f;  // px на sec
 int WIGHT = 800;
 int HEIGHT = 800;
+
+float C = 0.1;  // Коэффициент сопративления воздуху
+float G = 9.8f;  // px на sec
+int dt = 1;  // Изменение времени в сек.
 
 
 int main() {
@@ -39,31 +41,35 @@ int main() {
         window.clear();
 
         sf::Vector2f position = shape.getPosition();
+        float a;
         float next_y;
         if (direction == VectorDirection::down) {
-            v += G;
-            next_y = position.y + v;
+            a = G;
+            v += a*(float)dt - C*v*(float)dt;
+            next_y = position.y+v;
 
-            if (next_y > (float)HEIGHT - shape.getRadius()*2) {  // Касание пола
+            if (next_y > (float)HEIGHT - shape.getRadius()*2) {
                 next_y = (float)HEIGHT - shape.getRadius()*2;
                 direction = VectorDirection::up;
             }
         } else {
-            v -= (G + C);
+            a = -G;
+            v = std::max(v + a*(float)dt - C*v*(float)dt, 0.0f);
 
-            if (v < 0) {  // Обнуление скорости
+            if (v == 0.0f) {
                 direction = VectorDirection::down;
-                v = 0;
             }
 
-            next_y = position.y - v;
+            next_y = position.y-v;
         }
+        std::cout << v << " px/s" << std::endl;
+
         shape.setPosition(position.x, next_y);
-        std::cout << "V: " << v << " px/s"  << std::endl;
+
 
         window.draw(shape);
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(dt));
         window.display();
     }
 
